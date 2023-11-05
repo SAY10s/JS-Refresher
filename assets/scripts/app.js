13a05b96e34bb87add934921ef69c140aa64a02f
SAY10s <nikodemmajzner@gmail.com>
2023-11-05 18:00:21 +0100

    basic operations on tables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,5 +9,23 @@ import * as util from "./util.js";
 function showKeyData(apiKey, expirationDate = "01-01-2000") {
   console.log("apiKey: " + apiKey + " expires at " + expirationDate);
 }
-
 showKeyData(util.key);
+
+//-----------------------------------------------------
+
+const hobbies = ["sports", "cooking", "reading"];
+
+hobbies.push("lifting");
+console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => {
+//   return item === "sports";
+// });
+
+const index = hobbies.findIndex((item) => item === "sports"); //zwraca 'true' jeżeli item = "sports" oraz zwraca 'false' jeżeli item != "sports"
+console.log("The index of sports in our array is: " + index);
+
+const editedHobbies = hobbies.map((item) => "My hobby is " + item + "!");
+const objectifiedHobbies = hobbies.map((item) => ({ text: item }));
+console.log(editedHobbies);
+console.table(objectifiedHobbies);
