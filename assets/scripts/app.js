de5b4e64ed4f7270cf42f73451dece0bb7722c50
SAY10s <nikodemmajzner@gmail.com>
2023-11-05 17:00:54 +0100

    import & export

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+// import { key } from "./util.js";
+// import { key, accessDate as expires} from "./util.js";
+// import apiKey from "./util.js";
+import * as util from "./util.js";
+
+console.log(util.accessDate);
+console.log(util.key);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+// export let apiKey = "241560ef-50d8-47b5-a8d2-60677fb19ed9";
+export default "241560ef-50d8-47b5-a8d2-60677fb19ed9";
+export let key = "241560ef-50d8-47b5-a8d2-60677fb19ed9";
+export let accessDate = "01-01-2024";
