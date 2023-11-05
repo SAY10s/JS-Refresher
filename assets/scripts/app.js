2c5977a98ee26d03529fb150839b1af5cf571f36
SAY10s <nikodemmajzner@gmail.com>
2023-11-05 17:15:37 +0100

    functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,5 +3,11 @@
 // import apiKey from "./util.js";
 import * as util from "./util.js";
 
-console.log(util.accessDate);
-console.log(util.key);
+// console.log(util.accessDate);
+// console.log(util.key);
+
+function showKeyData(apiKey, expirationDate = "01-01-2000") {
+  console.log("apiKey: " + apiKey + " expires at " + expirationDate);
+}
+
+showKeyData(util.key);
